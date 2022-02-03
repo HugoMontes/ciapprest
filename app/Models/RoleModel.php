24f1b1d064a3c01108babd95e5256253bb27ffa3
HugoMontes <hugomontes927@gmail.com>
2022-02-03 02:12:14 -0400

    Inicio del proyecto

---
+++ app/Models/RoleModel.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class RoleModel extends Model
+{
+    protected $table      = 'roles';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = ['name'];
+
+    protected $useTimestamps = false;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+
+    public function get($id = null){
+        if($id === null){
+            return $this->findAll();
+        }
+        return $this->asArray()
+            ->where(['id'=>$id])
+            ->first();
+    }
+}
+
+
